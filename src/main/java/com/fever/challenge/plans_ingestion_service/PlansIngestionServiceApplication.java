6147bca590459902dcea1460502909eec0a66804
Juanpi <juanpablomore58@gmail.com>
2025-09-11 05:22:24 +0200

    Initial commit

---
+++ src/main/java/com/fever/challenge/plans_ingestion_service/PlansIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fever.challenge.plans_ingestion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlansIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlansIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fever/challenge/plans_ingestion_service/PlansIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fever.challenge.plans_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlansIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
